2afaee62e423b2afe647e23a4319f8b3d86f2894
Maciej Borzecki <maciej.zenon.borzecki@canonical.com>
2021-07-09 13:26:01 +0200

    cmd/devcgroupv2: WIP
    Signed-off-by: Maciej Borzecki <maciej.zenon.borzecki@canonical.com>

--- cmd/devcgroupv2/main.c
+++ cmd/devcgroupv2/main.c
@@ -17,14 +17,23 @@
 
 #define _GNU_SOURCE
 
+#include <stddef.h>
 #include <bpf/bpf.h>
 #include <bpf/libbpf.h>
 #include <fcntl.h>
 #include <linux/bpf.h>
+#include "bpf_insn.h"
 #include <stdint.h>
 #include <stdio.h>
 
 #include "../libsnap-confine-private/utils.h"
+
+static size_t align_to(size_t val, size_t alignment) {
+	if (val % alignment == 0) {
+		return val;
+	}
+	return alignment * ((val + alignment) / alignment);
+}
 
 int main(int argc, char **argv) {
     char log_buf[4096] = {0};
@@ -46,9 +55,70 @@ int main(int argc, char **argv) {
         die("cannot obtain map fd");
     }
 
+    // Basic rules about registers:
+    // r0    - return value of built in functions and exit code of the program
+	// r1-r5 - respective arguments to built in functions, clobbered by calls
+	// r6-r9 - general purpose, preserved by callees
+	// r10   - read only, stack pointer
+	// Stack is 512 bytes.
+	//
+    // The function declaration implementing the program looks like this:
+    // int program(struct bpf_cgroup_dev_ctx * ctx)
+    // where *ctx is passed in r1, while the result goes to r0
+	//
+	// The map holding device entries has the following keys:
+	struct key {
+        uint8_t  type;
+	    uint32_t major;
+	    uint32_t minor;
+    } __attribute__((packed));
+	/* just a placeholder for map value */
+	uint8_t map_value __attribute__((unused));
+	// where the value is 1 byte, but effectively ignored at this time. We are
+	// using the map as a set, but 0 sized key cannot be used when creating a
+	// map.
     struct bpf_insn prog[] = {
-		/* fill */
-    };
+		/* r1 holds pointer to bpf_cgroup_dev_ctx */
+
+		/* make some place on the stack for the key */
+		BPF_MOV64_REG(BPF_REG_6, BPF_REG_10), /* r6 = r10 (sp) */
+		BPF_ALU64_IMM(BPF_SUB, BPF_REG_6, align_to(sizeof(struct key), 4)), /* r6 = sp - sizeof(struct key) */
+		/* copy major to our key */
+		BPF_LDX_MEM(BPF_W, BPF_REG_2, BPF_REG_1,
+					offsetof(struct bpf_cgroup_dev_ctx, major)), /* r2 = *(u32)(r1->major) */
+		BPF_STX_MEM(BPF_W, BPF_REG_6, BPF_REG_2,
+					offsetof(struct key, major)), /* *(r6 + offsetof(major)) = r2 */
+		/* copy minor to our key */
+		BPF_LDX_MEM(BPF_W, BPF_REG_2, BPF_REG_1,
+					offsetof(struct bpf_cgroup_dev_ctx, minor)), /* r2 = *(u32)(r1->minor) */
+		BPF_STX_MEM(BPF_W, BPF_REG_6, BPF_REG_2,
+					offsetof(struct key, minor)), /* *(r6 + offsetof(minor)) = r2 */
+		/* copy device access_type to r2 */
+		BPF_LDX_MEM(BPF_W, BPF_REG_2, BPF_REG_1,
+					offsetof(struct bpf_cgroup_dev_ctx, minor)), /* r2 = *(u32*)(r1->access_type) */
+		/* access_type is encoded as (BPF_DEVCG_ACC_* << 16) | BPF_DEVCG_DEV_*,
+		 * but we only care about type */
+		BPF_ALU32_IMM(BPF_AND, BPF_REG_2, 0xffff), /* r2 = r2 & 0xffff */
+		/* is it a block device? */
+		BPF_JMP_IMM(BPF_JNE, BPF_REG_2, BPF_DEVCG_DEV_BLOCK, 2), /* if (r2 != BPF_DEVCG_DEV_BLOCK) goto pc + 2 */
+		BPF_ST_MEM(BPF_B, BPF_REG_6, offsetof(struct key, type), 'b'), /* *(uint8*)(r6->type) = 'b' */
+		BPF_JMP_A(4),
+		BPF_JMP_IMM(BPF_JNE, BPF_REG_2, BPF_DEVCG_DEV_CHAR, 2), /* if (r2 != BPF_DEVCG_DEV_CHAR) goto pc + 2 */
+		BPF_ST_MEM(BPF_B, BPF_REG_6, offsetof(struct key, type), 'c'), /* *(uint8*)(r6->type) = 'c' */
+		BPF_JMP_A(2),
+		/* unknown device type */
+        BPF_MOV64_IMM(BPF_REG_0, 0), /* r0 = 0 */
+		BPF_EXIT_INSN(),
+		/* back on happy path, prepare arguments for map lookup */
+		BPF_MOV64_IMM(BPF_REG_1, map_fd),
+		BPF_MOV64_REG(BPF_REG_2, BPF_REG_6), /* r2 = (struct key *) r6, */
+		BPF_RAW_INSN(BPF_JMP | BPF_CALL, 0, 0, 0, BPF_FUNC_map_lookup_elem), /* r0 = bpf_map_lookup_elem(<map>, &key) */
+		BPF_JMP_IMM(BPF_JEQ, BPF_REG_0, 0, 2), /* if (value_ptr == 0) goto pc + 2*/
+        BPF_MOV64_IMM(BPF_REG_0, 1), /* r0 = 1 */
+		BPF_JMP_A(1),
+        BPF_MOV64_IMM(BPF_REG_0, 0), /* r0 = 0 */
+		BPF_EXIT_INSN(),
+	};
 
     int verify = bpf_verify_program(BPF_PROG_TYPE_CGROUP_DEVICE, prog, sizeof(prog) / sizeof(prog[0]), 0, "GPL", 0,
                                     log_buf, sizeof(log_buf), 1);
