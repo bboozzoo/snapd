f5184d7e11eac6baa36b66c365109dfe76898c6b
Maciej Borzecki <maciej.zenon.borzecki@canonical.com>
2021-11-09 12:57:46 +0100

    o/snapstate: install fake handler for undo of foreign tasks too
    Signed-off-by: Maciej Borzecki <maciej.zenon.borzecki@canonical.com>

--- overlord/snapstate/snapstate_update_test.go
+++ overlord/snapstate/snapstate_update_test.go
@@ -2384,6 +2384,11 @@ func (s *snapmgrTestSuite) TestUpdateTotalUndoRunThrough(c *C) {
 			name: "some-snap",
 		},
 		{
+			op:    "auto-connect:Undoing",
+			name:  "some-snap",
+			revno: snap.R(11),
+		},
+		{
 			op:   "unlink-snap",
 			path: filepath.Join(dirs.SnapMountDir, "some-snap/11"),
 		},
