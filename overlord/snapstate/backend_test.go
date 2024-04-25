3b7a86f494d55638a963f80a9daf576ba1a93e7b
Maciej Borzecki <maciej.borzecki@canonical.com>
2024-04-25 12:10:56 +0200

    o/snapstate: fix race in fake backend operations
    Fix a race when appending an operations during failure scenarios in fake
    backend's LinkSnap.
    
    Reported by -race:
    
    ```
    ==================
    WARNING: DATA RACE
    Read at 0x00c0004822c0 by goroutine 1597:
      github.com/snapcore/snapd/overlord/snapstate_test.(*fakeSnappyBackend).appendOp()
          /home/runner/work/snapd/snapd/src/github.com/snapcore/snapd/overlord/snapstate/backend_test.go:1506 +0x104
      github.com/snapcore/snapd/overlord/snapstate_test.(*fakeSnappyBackend).ForeignTask()
          /home/runner/work/snapd/snapd/src/github.com/snapcore/snapd/overlord/snapstate/backend_test.go:1416 +0x1c4
      github.com/snapcore/snapd/overlord/snapstate_test.AddForeignTaskHandlers.func1()
          /home/runner/work/snapd/snapd/src/github.com/snapcore/snapd/overlord/snapstate/snapstate_test.go:366 +0x118
      github.com/snapcore/snapd/overlord/state.(*TaskRunner).run.func1()
          /home/runner/work/snapd/snapd/src/github.com/snapcore/snapd/overlord/state/taskrunner.go:220 +0xa2
      gopkg.in/tomb%2ev2.(*Tomb).run()
          /home/runner/work/snapd/snapd/src/github.com/snapcore/snapd/vendor/gopkg.in/tomb.v2/tomb.go:163 +0x3b
      gopkg.in/tomb%2ev2.(*Tomb).Go.gowrap2()
          /home/runner/work/snapd/snapd/src/github.com/snapcore/snapd/vendor/gopkg.in/tomb.v2/tomb.go:159 +0x44
    
    Previous write at 0x00c0004822c0 by goroutine 1598:
      github.com/snapcore/snapd/overlord/snapstate_test.(*fakeSnappyBackend).LinkSnap()
          /home/runner/work/snapd/snapd/src/github.com/snapcore/snapd/overlord/snapstate/backend_test.go:1163 +0x484
      github.com/snapcore/snapd/overlord/snapstate.(*SnapManager).doLinkSnap()
          /home/runner/work/snapd/snapd/src/github.com/snapcore/snapd/overlord/snapstate/handlers.go:2213 +0x1ca7
      github.com/snapcore/snapd/overlord/snapstate.(*SnapManager).doLinkSnap-fm()
          <autogenerated>:1 +0x47
      github.com/snapcore/snapd/overlord/state.(*TaskRunner).run.func1()
          /home/runner/work/snapd/snapd/src/github.com/snapcore/snapd/overlord/state/taskrunner.go:220 +0xa2
      gopkg.in/tomb%2ev2.(*Tomb).run()
          /home/runner/work/snapd/snapd/src/github.com/snapcore/snapd/vendor/gopkg.in/tomb.v2/tomb.go:163 +0x3b
      gopkg.in/tomb%2ev2.(*Tomb).Go.gowrap2()
          /home/runner/work/snapd/snapd/src/github.com/snapcore/snapd/vendor/gopkg.in/tomb.v2/tomb.go:159
          +0x44
    ```
    
    Signed-off-by: Maciej Borzecki <maciej.borzecki@canonical.com>

--- overlord/snapstate/backend_test.go
+++ overlord/snapstate/backend_test.go
@@ -675,7 +675,7 @@ func (f *fakeStore) SnapAction(ctx context.Context, currentSnaps []*store.Curren
 			}
 			hit = info.Revision
 		}
-		f.fakeBackend.ops = append(f.fakeBackend.ops, fakeOp{
+		f.fakeBackend.appendOp(&fakeOp{
 			op:     "storesvc-snap-action:action",
 			action: *a,
 			revno:  hit,
@@ -1150,7 +1150,7 @@ func (f *fakeSnappyBackend) LinkSnap(info *snap.Info, dev snap.Device, linkCtx b
 		vitalityRank = linkCtx.ServiceOptions.VitalityRank
 	}
 
-	op := fakeOp{
+	op := &fakeOp{
 		op:   "link-snap",
 		path: info.MountDir(),
 
@@ -1160,11 +1160,11 @@ func (f *fakeSnappyBackend) LinkSnap(info *snap.Info, dev snap.Device, linkCtx b
 
 	if info.MountDir() == f.linkSnapFailTrigger {
 		op.op = "link-snap.failed"
-		f.ops = append(f.ops, op)
+		f.appendOp(op)
 		return boot.RebootInfo{RebootRequired: false}, errors.New("fail")
 	}
 
-	f.appendOp(&op)
+	f.appendOp(op)
 
 	reboot := false
 	if f.linkSnapMaybeReboot {
@@ -1340,7 +1340,7 @@ func (f *fakeSnappyBackend) RemoveSnapSaveData(info *snap.Info, _ snap.Device) e
 }
 
 func (f *fakeSnappyBackend) RemoveSnapDataDir(info *snap.Info, otherInstances bool, opts *dirs.SnapDirOptions) error {
-	f.ops = append(f.ops, fakeOp{
+	f.appendOp(&fakeOp{
 		op:             "remove-snap-data-dir",
 		name:           info.InstanceName(),
 		path:           snap.BaseDataDir(info.SnapName()),
@@ -1350,7 +1350,7 @@ func (f *fakeSnappyBackend) RemoveSnapDataDir(info *snap.Info, otherInstances bo
 }
 
 func (f *fakeSnappyBackend) RemoveContainerMountUnits(s snap.ContainerPlaceInfo, meter progress.Meter) error {
-	f.ops = append(f.ops, fakeOp{
+	f.appendOp(&fakeOp{
 		op:   "remove-snap-mount-units",
 		name: s.ContainerName(),
 	})
@@ -1358,7 +1358,7 @@ func (f *fakeSnappyBackend) RemoveContainerMountUnits(s snap.ContainerPlaceInfo,
 }
 
 func (f *fakeSnappyBackend) RemoveSnapDir(s snap.PlaceInfo, otherInstances bool) error {
-	f.ops = append(f.ops, fakeOp{
+	f.appendOp(&fakeOp{
 		op:             "remove-snap-dir",
 		name:           s.InstanceName(),
 		path:           snap.BaseDir(s.SnapName()),
