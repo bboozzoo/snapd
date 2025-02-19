e562f0016ec4f494e705b72e30240bd06e0eb2b6
Maciej Borzecki <maciej.borzecki@canonical.com>
2025-02-19 13:52:37 +0100

    fixup! cmd/libsnap-confine-private: mkdir(at) & chmod helpers
    Signed-off-by: Maciej Borzecki <maciej.borzecki@canonical.com>

--- cmd/libsnap-confine-private/utils.c
+++ cmd/libsnap-confine-private/utils.c
@@ -279,13 +279,14 @@ static bool _sc_is_in_container(const char *p) {
 bool sc_is_in_container(void) { return _sc_is_in_container(run_systemd_container); }
 
 int sc_ensure_mkdirat(int fd, const char *name, mode_t mode, uid_t uid, uid_t gid) {
-    if (mkdirat(fd, name, mode) < 0) {
+    if (mkdirat(fd, name, 0000) < 0) {
         if (errno != EEXIST) {
             return -1;
         }
     } else {
         // new directory: set the right permissions and mode
-        if (fchownat(fd, name, uid, gid, AT_SYMLINK_NOFOLLOW) < 0) {
+        if (fchownat(fd, name, uid, gid, AT_SYMLINK_NOFOLLOW) < 0 ||
+            fchmodat(fd, name, mode, AT_SYMLINK_NOFOLLOW) < 0) {
             return -1;
         }
     }
@@ -293,13 +294,13 @@ int sc_ensure_mkdirat(int fd, const char *name, mode_t mode, uid_t uid, uid_t gi
 }
 
 int sc_ensure_mkdir(const char *path, mode_t mode, uid_t uid, uid_t gid) {
-    if (mkdir(path, mode) < 0) {
+    if (mkdir(path, 0000) < 0) {
         if (errno != EEXIST) {
             return -1;
         }
     } else {
         // new directory: set the right permissions and mode
-        if (chown(path, uid, gid) < 0) {
+        if (chown(path, uid, gid) < 0 || chmod(path, mode)) {
             return -1;
         }
     }
