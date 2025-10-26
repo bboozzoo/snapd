c22cae78939677083d40c1e6eba9d29f459cf9d2
Maciej Borzecki <maciej.borzecki@canonical.com>
2025-10-26 22:31:08 +0100

    cmd/libsnap-confine-private-rs: adding debian matcher WIP
    Signed-off-by: Maciej Borzecki <maciej.borzecki@canonical.com>

--- cmd/libsnap-confine-private-rs/src/distro.rs
+++ cmd/libsnap-confine-private-rs/src/distro.rs
@@ -63,8 +63,21 @@ pub fn classify(os_release: &Path, expected_meta_snap_yaml: &Path) -> Result<Dis
     }
 }
 
-pub fn is_debian_like() -> Result<bool> {
+pub fn is_debian_like(os_release: &Path) -> Result<bool> {
+    // let f = File::open(os_release);
+    // if let Ok(f) = f {
+    //     BufReader::new(f)
+    //         .lines()
+    //         .take_while(|l| match l {
+    //             r#"ID="debian""# | r#"ID=debian"# => true,
+    //             r#"ID_LIKE="debian""# | r#"ID_LIKE=debian"# => true,
+    //             _ => false,
+    //         })
+    //         .collect();
+    // } else {
+    //     eprintln!("error {}", f.err().unwrap());
     Err(io::Error::new(io::ErrorKind::Other, "not implemnted"))
+    // }
 }
 
 #[cfg(test)]
@@ -121,5 +134,179 @@ ID_LIKE=debian
         let ff = mock_files(Some(os_release_classic), None);
         let res = classify(&ff.os_release, &ff.meta_snap_yaml);
         assert_eq!(res.unwrap(), DistroKind::Classic)
+    }
+
+    #[test]
+    fn test_is_on_core_on16() {
+        let os_release_core16 = r#"
+NAME="Ubuntu Core"
+VERSION_ID="16"
+ID=ubuntu-core
+"#[1..]
+            .as_ref();
+
+        let meta_snap_yaml_core16 = r#"
+name: core
+version: 16-something
+type: core
+architectures: [amd64]
+        "#[1..]
+            .as_ref();
+        let ff = mock_files(Some(os_release_core16), Some(meta_snap_yaml_core16));
+        let res = classify(&ff.os_release, &ff.meta_snap_yaml);
+        assert_eq!(res.unwrap(), DistroKind::Core16);
+    }
+
+    #[test]
+    fn test_is_on_core_on18() {
+        let os_release_core18 = r#"
+NAME="Ubuntu Core"
+VERSION_ID="18"
+ID=ubuntu-core
+"#[1..]
+            .as_ref();
+
+        let meta_snap_yaml_core18 = r#"
+name: core18
+type: base
+architectures: [amd64]
+        "#[1..]
+            .as_ref();
+        let ff = mock_files(Some(os_release_core18), Some(meta_snap_yaml_core18));
+        let res = classify(&ff.os_release, &ff.meta_snap_yaml);
+        assert_eq!(res.unwrap(), DistroKind::CoreOther);
+    }
+
+    #[test]
+    fn test_is_on_core_on20() {
+        let os_release_core20 = r#"
+NAME="Ubuntu Core"
+VERSION_ID="20"
+ID=ubuntu-core
+"#[1..]
+            .as_ref();
+
+        let meta_snap_yaml_core20 = r#"
+name: core20
+type: base
+architectures: [amd64]
+        "#[1..]
+            .as_ref();
+        let ff = mock_files(Some(os_release_core20), Some(meta_snap_yaml_core20));
+        let res = classify(&ff.os_release, &ff.meta_snap_yaml);
+        assert_eq!(res.unwrap(), DistroKind::CoreOther);
+    }
+
+    #[test]
+    fn test_is_on_classic_with_long_line() {
+        let os_release_classic = r#"
+NAME=Ubuntu"
+VERSION="17.04 (Zesty Zapus)"
+ID=ubuntu
+ID_LIKE=debian
+LONG=line.line.line.line.line.line.line.line.line.line.line.line.line.line.line.line.line.line.line.line.line.line.line.line.line.line.line.line.line.line.line.line.line.line.line.line.line.line.line.line.line.line.line.line.line.line.line.line.line.line.line.line.
+"#[1..]
+            .as_ref();
+
+        let ff = mock_files(Some(os_release_classic), None);
+        let res = classify(&ff.os_release, &ff.meta_snap_yaml);
+        assert_eq!(res.unwrap(), DistroKind::Classic)
+    }
+
+    #[test]
+    fn test_is_on_fedora_base() {
+        let os_release_fedora_base = r#"
+NAME=Fedora
+ID=fedora
+VARIANT_ID=snappy
+"#[1..]
+            .as_ref();
+
+        let meta_snap_yaml_fedora_base = r#"
+name: fedora29
+type: base
+architectures: [amd64]
+        "#[1..]
+            .as_ref();
+        let ff = mock_files(
+            Some(os_release_fedora_base),
+            Some(meta_snap_yaml_fedora_base),
+        );
+        let res = classify(&ff.os_release, &ff.meta_snap_yaml);
+        assert_eq!(res.unwrap(), DistroKind::CoreOther);
+    }
+
+    const os_release_fedora_ws: &str = r#"
+NAME=Fedora
+ID=fedora
+VARIANT_ID=workstation
+"#;
+
+    #[test]
+    fn test_is_on_fedora_ws() {
+        let ff = mock_files(Some(&os_release_fedora_ws[1..]), None);
+        let res = classify(&ff.os_release, &ff.meta_snap_yaml);
+        assert_eq!(res.unwrap(), DistroKind::Classic)
+    }
+
+    #[test]
+    fn test_is_on_custom_base() {
+        let os_release_custom = r#"
+NAME="Custom Distribution"
+ID=custom
+"#[1..]
+            .as_ref();
+
+        let ff = mock_files(Some(os_release_custom), None);
+        let res = classify(&ff.os_release, &ff.meta_snap_yaml);
+        assert_eq!(res.unwrap(), DistroKind::Classic);
+
+        let meta_snap_yaml_custom = r#"
+name: custom
+version: rolling
+summary: Runtime environment based on Custom Distribution
+type: base
+architectures: [amd64]
+        "#[1..]
+            .as_ref();
+        let ff = mock_files(Some(os_release_custom), Some(meta_snap_yaml_custom));
+        let res = classify(&ff.os_release, &ff.meta_snap_yaml);
+        assert_eq!(res.unwrap(), DistroKind::CoreOther);
+    }
+
+    #[test]
+    fn test_is_debian_like() {
+        let os_release_debian_like_valid = r#"
+ID=my-fun-distro
+ID_LIKE=debian
+"#[1..]
+            .as_ref();
+
+        let ff = mock_files(Some(os_release_debian_like_valid), None);
+        assert!(is_debian_like(&ff.os_release).unwrap());
+
+        let os_release_debian_like_quoted_valid = r#"
+ID=my-fun-distro
+ID_LIKE="debian"
+"#[1..]
+            .as_ref();
+
+        let ff = mock_files(Some(os_release_debian_like_quoted_valid), None);
+        assert!(is_debian_like(&ff.os_release).unwrap());
+
+        let os_release_actual_debian_valid = r#"
+ID=debian
+"#[1..]
+            .as_ref();
+        let ff = mock_files(Some(os_release_actual_debian_valid), None);
+        assert!(is_debian_like(&ff.os_release).unwrap());
+
+        let ff = mock_files(Some(&os_release_fedora_ws[1..]), None);
+        assert!(!is_debian_like(&ff.os_release).unwrap());
+
+        let os_release_invalid = "garbage\n";
+
+        let ff = mock_files(Some(os_release_invalid), None);
+        assert!(!is_debian_like(&ff.os_release).unwrap());
     }
 }
